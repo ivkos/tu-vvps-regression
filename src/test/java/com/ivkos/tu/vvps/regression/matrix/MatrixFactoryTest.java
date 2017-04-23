1f365c6ae5051e142bcdd130fb4a0387b177d98b
Ivaylo Stoyanov <me@ivkos.com>
2017-04-23 15:57:05 +0300

    Update MatrixFactoryTest

--- src/test/java/com/ivkos/tu/vvps/regression/matrix/MatrixFactoryTest.java
+++ src/test/java/com/ivkos/tu/vvps/regression/matrix/MatrixFactoryTest.java
@@ -1,36 +1,44 @@
 package com.ivkos.tu.vvps.regression.matrix;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
 public class MatrixFactoryTest
 {
-   @Test
-   public void create() throws Exception
-   {
-      MatrixFactory mf = new MatrixFactory();
+   private AbstractMatrixFactory mf;
 
+   @Before
+   public void setUp()
+   {
+      mf = new MatrixFactory();
+      assertNotNull(mf);
+   }
+
+   @Test
+   public void create1D() throws Exception
+   {
       Matrix mx1 = mf.create(new double[] { 1, 2, 3 });
       assertNotNull(mx1);
+   }
 
+   @Test
+   public void create2D() throws Exception
+   {
       Matrix mx2 = mf.create(new double[][] { { 1, 2, 3 }, { 4, 5, 6 } });
       assertNotNull(mx2);
    }
 
    @Test(expected = NullPointerException.class)
-   public void createWithNull() throws Exception
+   public void createWithNull1D() throws Exception
    {
-      MatrixFactory mf = new MatrixFactory();
-
       mf.create((double[]) null);
    }
 
    @Test(expected = NullPointerException.class)
-   public void createWithNull2() throws Exception
+   public void createWithNull2D() throws Exception
    {
-      MatrixFactory mf = new MatrixFactory();
-
       mf.create((double[][]) null);
    }
 }
